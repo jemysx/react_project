e9cf21d35c25c1308498b7903f4cef79db384a3a
Ken <flh070147000303@gmail.com>
2023-03-08 17:33:13 +0900

    关于headers发送的authorization,后端无法接收的问题

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component} from 'react'
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
 import Login from './components/Login'
+import Users from './components/Users'
 export default class App extends Component {
 
   render() {
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,142 +1,188 @@
-import React, { Component } from 'react'
-import qs from "qs"
+import React, {useState} from 'react';
+import qs from "qs";
 import axios from 'axios';
-export default class Login extends Component {
- 
-   //初始化状态
-    
-    state = {
-      username:'',
-      isFirst:true,  //是否为第一次打开页面
-      isLoading:false, //标识是否处于加载中
-      err:"",  //存储请求相关的错误信息
-      isLogin:false  //是否登录
-    }
-    //用几个就要创建几个ref
-    userNameRef = React.createRef()
-    passWordRef = React.createRef()
-    
-    //用户登录
-    loading = async(event)=>{
-      //阻止冒泡事件
-      event.preventDefault();
-      const {userNameRef:{current:{value:username}}} =this
-      const {passWordRef:{current:{value:password}}} =this
-    
 
-      if(username.trim() === ""|| password.trim() === ""){
-         alert("用户名或密码不能为空");
-         return false;
-      }
-      this.setState({isFirst:false})
-      this.setState({username:username})
-      
-      //  console.log("用户名",username)
-      //  console.log("密码",password)
-      //发送请求
-      try{
-           // 发送登录请求，获取 JWT
-            const options = {
-            method: 'POST',
-            headers: { 'content-type': 'application/x-www-form-urlencoded' },
-            data: qs.stringify({ username, password }),
-            // url:'http://mysite.io/api/login.php'
-            url:'http://localhost:3000/api1/api/login.php'
-          };
-          const response = await axios(options);
-          console.log("返回的数据",response)
-          if(response.status === 200){
-            //改变登录状态
-            this.setState({isLogin:true})
-            console.log("返回的data",response.data.jwt)
-            const {jwt:token} = response.data
-            
-            localStorage.setItem('jwtToken', token);
-          }
-       
+//用函数组件进行重构
+function Login() {
   
-          
-      }catch(error){
-        console.log('走这儿了?')
-         this.setState({err:error.response.data.message})
+    const[username,setUsername] = useState('');
+    const[password,setPassword] = useState('');
+    const[token,setToken] = useState('');
+    const[err,setErr] = useState('');
+    //默认是登录
+    const [authMode, setAuthMode] = useState("signin")
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization':'BearerJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1c2VyIiwiYXVkIjoiU1VOIFBFTkciLCJpYXQiOjE2NzgyNjA3OTYsImV4cCI6MTY3ODI2NDM5Niwic3ViIjoiMSJ9.Tgh8tMf0mLRAHeUVvUo5k_Hnt01diq92AiNvHX49CXI'
       }
+    };
+    
+    //创建一个axios实例
+    const axiosInstance = axios.create({
+      baseURL:'http://localhost:3000',
+      timeout:1000
+    });
 
-    }
-
-    //用户注册
-    register = async(event)=>{
-        //阻止冒泡事件
-      event.preventDefault();
-      //解构赋值+重命名
-      const {userNameRef:{current:{value:username}}} =this
-      const {passWordRef:{current:{value:password}}} =this
-      if(username.trim() === ""|| password.trim() === ""){
-        alert("用户名或密码不能为空");
-        return false;
-     }
-      this.setState({username:username})
-      this.setState({password:password})
-      this.setState({isFirst:false})
-      //发送请求
-            try{
-              // 发送注册请求，获取 JWT
-               const options = {
-               method: 'POST',
-               headers: {'content-type': 'application/x-www-form-urlencoded'},
-               data: qs.stringify({ username, password }),
-               // url:'http://mysite.io/api/register.php'
-               url:'http://localhost:3000/api1/api/register.php'
-             };
-             const response = await axios(options);
-             console.log("返回的数据",response) 
-             if(response.status === 200){
-              //改变登录状态
-              this.setState({isLogin:true})
-              console.log("返回的data",response.data.jwt)
-              const {jwt:token} = response.data
-              alert('注册成功');
-              localStorage.setItem('jwtToken', token);
-
-            }
-         }catch(error){
-            console.log("注册的",error);
-            this.setState({err:error.response.data.message})
+    //添加一个请求拦截器
+    axiosInstance.interceptors.request.use(
+      config=>{
+         const token = localStorage.getItem('token');
+         console.log("拦截器",token);
+         if(token){
+          config.headers.Authorization = `Bearer ${token}`;
          }
-  }
-
-
-  render() {
-    const {isFirst,err,isLogin,username}  = this.state
+         return config;
+      },
+      error=>{
+        //处理请求错误
+        return Promise.reject(error)
+      }
+    );
 
     
+     //登录请求
+    const handleLogin = async(event)=>{
+    
+      //阻止冒泡
+      event.preventDefault();
+      console.log("头部信息",config);
+         //做验证
+     
+      // console.log("请求其返回的",config)
+      axiosInstance.post('/api1/api/login.php',{
+          username:username,
+          password:password
+      },config).then(response=>{
+          // console.log("shili",response)
+          setToken(response.data.jwt);
+          localStorage.setItem('token',response.data.jwt)
+      })
+      .catch(error=>{
+        //  console.log(error)
+         setErr(error.response.data.message)
+      })
+    }
+
+
+   //更改changeAuthMode的状态
+   const changeAuthMode =()=>{
+       setAuthMode(authMode === "signin" ? "signup" : "signin")
+   }
+    //注册请求
+   const handleRegister = (event)=>{
+      //阻止冒泡
+      event.preventDefault();
+
+      //做验证
+      if (username.length < 8 || password.length<8) {
+        setErr('用户名或者密码不能少于8位');
+        return false;
+      } else if (/[!@#$%^&*(),.?":{}|<>]/g.test(username) ||/[!@#$%^&*(),.?":{}|<>]/g.test(password)) {
+        setErr('用户名或者密码不能使用特殊符号');
+        return false;
+      }
+
+      //  console.log('zhuce');
+      axiosInstance.post('/api1/api/register.php',{
+        username:username,
+        password:password
+      },config).then(response=>{
+          console.log("注册",response)
+          setToken(response.data.jwt);
+          localStorage.setItem('token',response.data.jwt)
+      })
+      .catch(error=>{
+         console.log(error)
+        setErr(error.response.data.message)
+      })
+   }
+
+
+
+
+    const loggedIn = ()=>{
+       const token = localStorage.getItem('token');
+       if(!token || token === 'undefined'){
+        return false;
+       }
+       return true;
+    }
+
+    const handleLogout =()=>{
+          //从本地存储中删除token
+          localStorage.removeItem('token');
+          setToken('')
+    }
+    if(authMode === "signin"){
+      return (
+        <div className="card">
+          <div className="card-body">
+              {loggedIn() ? (
+              <div>
+                <p>You are logged in.</p>
+                <button onClick={handleLogout}>Logout</button>
+              </div>
+            ) :
+              <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                  <div className="text-center">
+                    还没有账号吗?{" "}
+                    <span className="link-primary" onClick={changeAuthMode}>
+                       注册
+                    </span>
+                  </div>
+                  <label htmlFor="exampleInputEmail1" className="form-label">用户名:</label>
+                  <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={username}  onChange={(e)=>{setUsername(e.target.value)}}/>
+                  {err ?   <div id="emailHelp" className="form-text">{err}</div> : <div id="emailHelp" className="form-text">我们不会泄露任何您的个人信息</div>}
+                 
+                  
+                 
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">密码:</label>
+                  <input type="password"className="form-control" id="exampleInputPassword1" value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+                </div>
+                <button type="submit" className="btn btn-dark btn-block btn-primary">登录</button> 
+              </form>}
+          </div>
+        </div>
+    )
+    }
     return (
       <div className="card">
         <div className="card-body">
-          {isLogin ? 
-          <>
-          <h1>欢迎{username}</h1>
-          <button>logout</button> 
-          </>: 
-           isFirst ?  <h1 className="card-title">欢迎使用</h1> :<></>}
-           {!isLogin ?
-            <form>
+            {loggedIn() ? (
+            <div>
+              <p>You are logged in.</p>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) :
+            <form onSubmit={handleRegister}>
               <div className="mb-3">
+                <div className="text-center">
+                  已经注册了??{" "}
+                  <span className="link-primary" onClick={changeAuthMode}>
+                     去登录
+                  </span>
+                </div>
                 <label htmlFor="exampleInputEmail1" className="form-label">用户名:</label>
-                <input type="text" ref={this.userNameRef} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                {
-                  err? <div id="emailHelp" className="form-text">{err}</div>:<div id="emailHelp" className="form-text">我们不会泄露任何您的个人信息</div>
-                }
+                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={username}  onChange={(e)=>{setUsername(e.target.value)}}/>
+                {err ?   <div id="emailHelp" className="form-text">{err}</div> : <div id="emailHelp" className="form-text">我们不会泄露任何您的个人信息</div>}
+               
+                
                
               </div>
               <div className="mb-3">
                 <label htmlFor="exampleInputPassword1" className="form-label">密码:</label>
-                <input type="password"ref={this.passWordRef} className="form-control" id="exampleInputPassword1"/>
+                <input type="password"className="form-control" id="exampleInputPassword1" value={password} onChange={(e)=>{setPassword(e.target.value)}} />
               </div>
-              <button type="submit" className="btn btn-dark btn-block btn-primary" onClick={this.loading}>登录</button>  <button type="submit" className="btn btn-dark btn-block btn-primary" onClick={this.register}>注册</button>
-            </form>
-            :<></>}
+              <button type="submit" className="btn btn-dark btn-block btn-primary">注册</button> 
+            </form>}
         </div>
       </div>
-    )
-  }
+  )
+  
 }
+
+export default Login;
